adf6ef5d5790b050506bdc30d1243d2159c3e101
Sharzil Haris Khan <51935396+sharzil1994@users.noreply.github.com>
2024-03-21 09:42:51 +0900

    Update windowing.py

--- windowing.py
+++ windowing.py
@@ -7,6 +7,7 @@ Created on Thu Mar  2 14:52:00 2023
 """
 import numpy as np
 
+
 def windowing(features,w_size):   
     to=0
     for index in features:
@@ -37,40 +38,6 @@ def windowing(features,w_size):
             l=l+1
     finalout1=finalout1.reshape((finalout1.shape[0], finalout1.shape[1],finalout1.shape[2], 1))
     return finalout1
-
-def windowing_avg(features,w_size):   
-    to=0
-    for index in features:
-        b=np.shape(index)[0]
-        to=to+b
-    
-        
-    
-    a=features[0]
-    fea_len=np.shape(a)[1]
-    finalout1=np.zeros([to,fea_len],'float')
-    
-    l=0
-    for i in range(0,len(features)):
-        temp_features=features[i]
-        
-        for j in range( 0, np.shape(temp_features)[0]):
-            
-            div=0
-            for k in range(0,w_size):
-                
-                k1=int(j+k-((w_size-1)/2))
-                
-                if k1<0 or k1 > np.shape(temp_features)[0]-1:
-                    pass
-                else:
-                    finalout1[l,:]=finalout1[l,:]+temp_features[k1,:]
-                    div=div+1
-            
-            finalout1[l,:]=finalout1[l,:]/div
-            l=l+1         
-    return finalout1
-
 
 
     
@@ -113,6 +80,3 @@ def Protein_seq_feature(seqs1,seqs4):
             d=np.concatenate((d,c),axis=0)
         seqs4[index]=d
     return seqs4
-    
-    
-    
